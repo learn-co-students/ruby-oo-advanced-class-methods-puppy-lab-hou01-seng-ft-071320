c0ca3ba9d076609cd8d12946c67e5890f05992a2
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2020-07-19 22:12:52 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog 
+    attr_accessor :name
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        self.save    
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear 
+    end
+    
+    def self.print_all
+        @@all.each {|index| puts index.name}
+    end
+    
+    def save
+        @@all << self
+    end
+
+
+
+    #not the end 
+end
